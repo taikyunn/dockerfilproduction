e368d05a2cac8ef8aaa442ef50018546afe29bb0
taikyunn <taichi_141@me.com>
2021-11-29 12:26:59 +0900

    commit

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	// "log"
-	// "os"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,18 +13,18 @@ type User struct {
 }
 
 func main() {
-	// port := os.Getenv("PORT")
+	port := os.Getenv("PORT")
 
-	// if port == "" {
-	// 	log.Fatal("$PORT must be set")
-	// }
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*.html")
 
 	r.GET("/", handler)
-	// router.Run(":" + port)
-	r.Run(":3000")
+	r.Run(":" + port)
+	// r.Run(":3000")
 }
 
 func handler(c *gin.Context) {
